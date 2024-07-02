6d989dcc3a9b5b1d3bad25215ea8394f7a92140b
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-02 16:13:20 +0400

    Delete stat directory

--- stat/client/src/main/java/exploreWithMe/Dto/repository/StatRepository.java
+++
@@ -1,7 +0,0 @@
-package exploreWithMe.Dto.repository;
-
-import exploreWithMe.Dto.model.Hit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatRepository extends JpaRepository<Integer, Hit> {
-}
--- stat/dto/src/main/java/ru/practicum/exploreWithMe/View/dto/ViewStats.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exploreWithMe.View.dto;
-
-public class ViewStats {
-    private String app;
-    private String uri;
-    private int hits;
-}
