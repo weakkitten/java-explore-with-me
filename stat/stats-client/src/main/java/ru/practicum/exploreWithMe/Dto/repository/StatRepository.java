0a6ee169ebb7ef3372cd0091ecdc91104c355753
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-11 16:29:55 +0400

    Update StatRepository.java

--- stat/stats-client/src/main/java/ru/practicum/exploreWithMe/Dto/repository/StatRepository.java
+++ stat/stats-client/src/main/java/ru/practicum/exploreWithMe/Dto/repository/StatRepository.java
@@ -16,16 +16,19 @@ public interface StatRepository extends JpaRepository<Hit, Integer> {
             "group by s.uri " +
             "order by hits desc", nativeQuery = true)
     List<ViewStatsInterface> getStats(LocalDateTime start, LocalDateTime end, List<String> uris);
+    
     @Query(value = "select distinct s.app, s.uri, count(distinct s.uri) as hits from stats as s " +
             "where s.times between ?1 and ?2 " +
             "and s.uri in (?3) " +
             "group by s.uri " +
             "order by hits desc", nativeQuery = true)
     List<ViewStatsInterface> getStatsUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
+    
     @Query(value = "select s.app, s.uri, count(s.uri) as hits from stats as s " +
             "where s.times between ?1 and ?2 " +
             "group by s.uri", nativeQuery = true)
     List<ViewStatsInterface> getStatsWithoutUris(LocalDateTime start, LocalDateTime end);
+    
     @Query(value = "select distinct s.app, s.uri, count(distinct s.uri) as hits from stats as s " +
             "where s.times between ?1 and ?2 " +
             "group by s.uri", nativeQuery = true)
