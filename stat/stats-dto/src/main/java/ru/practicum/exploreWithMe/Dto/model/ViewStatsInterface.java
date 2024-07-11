602ae261b137da520bf5fd6ee9164945eae2bf8d
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-11 15:18:11 +0400

    Update ViewStatsInterface.java

--- stat/stats-dto/src/main/java/ru/practicum/exploreWithMe/Dto/model/ViewStatsInterface.java
+++ stat/stats-dto/src/main/java/ru/practicum/exploreWithMe/Dto/model/ViewStatsInterface.java
@@ -2,6 +2,8 @@ package ru.practicum.exploreWithMe.Dto.model;
 
 public interface ViewStatsInterface {
         String getApp();
+        
         String getUri();
+        
         Integer getHits();
 }
