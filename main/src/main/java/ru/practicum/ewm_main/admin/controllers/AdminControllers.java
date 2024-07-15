9fc553c09e58eb559930ec8496f817e30572cdc5
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-15 14:46:37 +0400

    Add files via upload

--- main/src/main/java/ru/practicum/ewm_main/admin/controllers/AdminControllers.java
+++ main/src/main/java/ru/practicum/ewm_main/admin/controllers/AdminControllers.java
@@ -2,16 +2,68 @@ package ru.practicum.ewm_main.admin.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm_main.admin.service.AdminService;
 
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping(path = "/admin")
 @Slf4j
 @RequiredArgsConstructor
 public class AdminControllers {
     private final AdminService service;
 
+    @PostMapping("/categories")
+    public void addNewCategories() {
 
+    }
+
+    @DeleteMapping("/categories/{catId}")
+    public void deleteCategories(@PathVariable int catId) {
+
+    }
+
+    @PatchMapping("/categories/{catId}")
+    public void updateCategories(@PathVariable int catId) {
+
+    }
+
+    @GetMapping("/events")
+    public void getEvents() {
+
+    }
+
+    @PatchMapping("/events/{eventId}")
+    public void updateEvents(@PathVariable int eventId) {
+
+    }
+
+    @GetMapping("/users")
+    public void getUsers() {
+
+    }
+
+    @PostMapping("/users")
+    public void addNewUser() {
+
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void deleteUser(@PathVariable int userId) {
+
+    }
+
+    @PostMapping("/compilations")
+    public void addNewCompilation() {
+
+    }
+
+    @DeleteMapping("/compilations/{compId}")
+    public void deleteCompilations(@PathVariable int compId) {
+
+    }
+
+    @PatchMapping("/compilations/{compId}")
+    public void updateCompilation() {
+
+    }
 }
