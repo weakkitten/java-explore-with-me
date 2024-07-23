bbeb45845570a4fcb6d9af4ccbe51a124ac7fdcc
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-23 14:57:19 +0400

    Delete main directory

--- main/src/main/java/ru/practicum/ewm_main/error/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm_main.error.exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(String message) {
-        super(message);
-    }
-}
