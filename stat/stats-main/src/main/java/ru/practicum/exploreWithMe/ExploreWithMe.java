d80a76b0b55cc7c0edc1127473aad4e105e6cb20
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-23 14:57:09 +0400

    Delete stat directory

--- stat/stats-main/src/main/java/ru/practicum/exploreWithMe/ExploreWithMe.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.exploreWithMe;
-
-import org.springframework.boot.SpringApplication;
-
-public class ExploreWithMe {
-	public static void main(String[] args) {
-		SpringApplication.run(ExploreWithMe.class, args);
-	}
-}
