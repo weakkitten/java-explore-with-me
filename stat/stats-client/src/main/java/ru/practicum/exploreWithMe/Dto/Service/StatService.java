0702fa66cae6c40ffa529fc19e6c2a9e23595dfd
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-23 15:35:50 +0400

    Add files via upload

---
+++ stat/stats-client/src/main/java/ru/practicum/exploreWithMe/Dto/Service/StatService.java
@@ -0,0 +1,53 @@
+package ru.practicum.exploreWithMe.Dto.Service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.practicum.exploreWithMe.Dto.error.exception.BadRequestException;
+import ru.practicum.exploreWithMe.Dto.repository.StatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.exploreWithMe.Dto.model.Hit;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatService {
+    private final StatRepository repository;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Transactional
+    public ResponseEntity<Hit> saveHit(Hit hit) {
+        repository.save(hit);
+        Hit hitDto = repository.findByIpAndTimes(hit.getIp(), hit.getTimestamp());
+        return ResponseEntity.status(HttpStatus.CREATED).body(hitDto);
+    }
+
+    public Object getStat(String start,
+                                   String end,
+                                   List<String> uris,
+                                   boolean unique) {
+        LocalDateTime startTime = LocalDateTime.parse(start, formatter);
+        LocalDateTime endTime = LocalDateTime.parse(end, formatter);
+        if (startTime.isAfter(endTime)) {
+            throw new BadRequestException("Некорректное время");
+        }
+        if (uris == null) {
+            if (unique) {
+                return repository.getStatsUniqueWithoutUris(startTime, endTime);
+            } else {
+                return repository.getStatsWithoutUris(startTime, endTime);
+            }
+        } else {
+            if (unique) {
+                return repository.getStatsUnique(startTime, endTime, uris);
+            } else {
+                System.out.println("Список - " + List.of(uris));
+                return repository.getStats(startTime, endTime, uris);
+            }
+        }
+    }
+}
---
+++ stat/stats-main/src/main/java/ru/practicum/exploreWithMe/Endpoint/HitController.java
@@ -0,0 +1,31 @@
+package ru.practicum.exploreWithMe.Endpoint;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.exploreWithMe.Dto.Service.StatService;
+import ru.practicum.exploreWithMe.Dto.model.Hit;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "")
+@Slf4j
+@RequiredArgsConstructor
+public class HitController {
+    private final StatService service;
+
+    @PostMapping("/hit")
+    public ResponseEntity<Hit> saveHit(@RequestBody Hit hit) {
+        log.info("Объект отправлен: " + hit);
+        return service.saveHit(hit);
+    }
+
+    @GetMapping("/stats")
+    public Object getStat(@RequestParam String start,
+                                            @RequestParam String end,
+                                            @RequestParam(required = false) List<String> uris,
+                                            @RequestParam(defaultValue = "false") boolean unique) {
+        return service.getStat(start, end, uris, unique);
+    }
+}
