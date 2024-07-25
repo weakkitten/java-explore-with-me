3ca36696bdc60cec7fc1e1b0f30131694693d78d
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-07-25 10:59:24 +0400

    Delete main directory

--- main/src/main/java/ru/practicum/ewm_main/categories/repository/CategoriesRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.ewm_main.categories.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.ewm_main.categories.model.Categories;
-
-public interface CategoriesRepository extends JpaRepository<Categories, Integer> {
-    public Categories findByName(String name);
-}
--- main/src/main/java/ru/practicum/ewm_main/utility/State.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm_main.utility;
-
-public enum State {
-    PENDING,
-    PUBLISHED,
-    CANCELED
-}
